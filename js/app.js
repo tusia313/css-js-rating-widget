c1c681c70e394e8f2f008d8d64f169466b22e8a1
Marta Łoskot <loskotmarta@gmail.com>
2024-01-15 11:57:31 +0100

    Create function and adding e.target.id

--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
 const mouth = document.querySelector('#mouth')
 const emotionElements = document.querySelectorAll('.rating-container div')
+console.log(emotionElements)
 
+function selectEmotion(e) {
+    console.log('clicked', e.target.id)
+}
+
+emotionElements.forEach(emotionElement =>
+    emotionElement.addEventListener('click', selectEmotion)
+)
 
 
 
